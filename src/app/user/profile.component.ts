17bb5e384ccb49f1271218050139c2fa6acbfa71
S Y <yaktsu@gmail.com>
2019-11-12 10:18:41 +0100

    Add profile component to help users edit profiles

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-    template: `
-        <div class="container">
-            <h1>{{ pageTitle }}</h1>
-            <hr>
-            <div class = "col-md-6">
-                <h3>[Profile Edit Form Goes Here]</h3>
-                <br><br>
-                <button type="submit" class="btn btn-primary" id="saveBtn">Save</button>
-                <button type="button" class="btn btn-secondary">Cancel</button>
-            </div>
-        </div>
-      `,
+    templateUrl: './profile.component.html',
     styles: [`
         .container {padding-top: 100px;}
         #saveBtn {padding-rigth: 10px;}
     `]
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
     // tslint:disable-next-line: no-inferrable-types
     pageTitle: string = 'Edit Your Profile';
+    profileForm: FormGroup;
+
+    ngOnInit() {
+        let firstname = new FormControl();
+        let lastname = new FormControl();
+        this.profileForm = new FormGroup({
+            firstname,
+            lastname
+        });
+    }
+
 }
